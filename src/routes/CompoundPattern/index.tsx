e0e189188e4eca0d263dfc8b0b18d21d30424e6b
Umut Palabiyik <57181469+UmutPalabiyik@users.noreply.github.com>
2023-08-24 12:29:25 +0300

    edit

--- src/routes/CompoundPattern/index.tsx
+++ src/routes/CompoundPattern/index.tsx
@@ -3,30 +3,30 @@ import { products } from "./data"
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ReportIcon from '@mui/icons-material/Report';
 import ModeCommentIcon from '@mui/icons-material/ModeComment';
+import { TravelerCardProps } from "./interfaces";
 
 const WithCompoundPattern = () => {
     return (
         <div className="p-16 flex flex-wrap justify-center gap-4">
             {
-                products.map((product: any) => {
-                    return (
-                        <TravelerCard
-                            product={product}
-                        >
-                            <TravelerCard.TravelerTopActions>
-                                <TravelerCard.TravelerTopAction icon={<FavoriteIcon />} />
-                                <TravelerCard.TravelerTopAction icon={<ReportIcon />} />
-                                <TravelerCard.TravelerTopAction icon={<ModeCommentIcon />} />
-                            </TravelerCard.TravelerTopActions>
-                            <TravelerCard.TravelerImage />
-                            <TravelerCard.TravelerCategory />
-                            <TravelerCard.TravelerTitle />
-                            <TravelerCard.TravelerRating />
-                            <TravelerCard.TravelerPrice currency="$" />
-                            <TravelerCard.TravelerAction />
-                        </TravelerCard>
-                    )
-                })
+                products.map((product: TravelerCardProps) => (
+                    <TravelerCard product={product}>
+
+                        <TravelerCard.TravelerTopActions>
+                            <TravelerCard.TravelerTopAction icon={<FavoriteIcon />} />
+                            <TravelerCard.TravelerTopAction icon={<ReportIcon />} />
+                            <TravelerCard.TravelerTopAction icon={<ModeCommentIcon />} />
+                        </TravelerCard.TravelerTopActions>
+
+                        <TravelerCard.TravelerImage />
+                        <TravelerCard.TravelerCategory />
+                        <TravelerCard.TravelerTitle />
+                        <TravelerCard.TravelerRating />
+                        <TravelerCard.TravelerPrice currency="$" />
+                        <TravelerCard.TravelerAction />
+
+                    </TravelerCard>
+                ))
             }
         </div>
 
